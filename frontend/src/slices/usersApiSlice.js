6c4e7e3104e271b97849aef2fcda6d7471e2d6a7
Ivan <ivinci.design@gmail.com>
2023-07-10 07:36:35 -0700

    47. Auth & User API Slice

---
+++ frontend/src/slices/usersApiSlice.js
@@ -0,0 +1,17 @@
+import { USERS_URL } from '../constants'
+import { apiSlice } from './apiSlice'
+
+export const usersApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    login: builder.mutation({
+      query: (data) => ({
+        url: USERS_URL / auth,
+        method: 'POST',
+        body: data
+      }),
+    }),
+  })
+})
+
+// useGetProductsQuery below is based on getProducts from above
+export const { useLoginMutation } = usersApiSlice
