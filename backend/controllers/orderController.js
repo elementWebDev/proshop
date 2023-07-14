daf2417802621ecd0e964c96b47e04250c772a87
Ivan <ivinci.design@gmail.com>
2023-07-14 13:28:21 -0700

    59. Creating An Order
    Issues students noticed...
    
    Not sure about others, but...
    On my end:
    - import Order from '../models/orderModel.js'

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,7 +1,7 @@
 import asyncHandler from '../middleware/asyncHandler.js'
-import order from '../models/orderModel.js'
+import Order from '../models/orderModel.js'
 
-// @desc    Create a new order
+// @desc    Create new order
 // @route   POST /api/orders
 // @access  Private
 const addOrderItems = asyncHandler(async (req, res) => {
--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken'
 
 const generateToken = (res, userId) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-    expiresIn: '30d'
+    expiresIn: '1h'
   })
 
   // Set JWT as HTTP-Only cookie
@@ -10,7 +10,8 @@ const generateToken = (res, userId) => {
     httpOnly: true,
     secure: process.env.NODE_ENV !== 'development', // true in production
     sameSite: 'strict',
-    maxAge: 30 * 24 * 60 * 60 * 1000 // 30 Days
+    // 14 days
+    maxAge: 14 * 24 * 60 * 60 * 1000
   })
 }
 
