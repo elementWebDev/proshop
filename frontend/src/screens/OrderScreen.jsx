e01105b87f4eb880ea8817988b0547cd45a5cc88
Ivan <ivinci.design@gmail.com>
2023-07-20 12:10:09 -0700

    69. List Products For Admin

--- frontend/src/screens/OrderScreen.jsx
+++ frontend/src/screens/OrderScreen.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react'
 import { Link, useParams } from 'react-router-dom'
-import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
+import { Row, Col, ListGroup, Image, Button, Card } from 'react-bootstrap'
 import { toast } from 'react-toastify'
 import { useSelector } from 'react-redux'
 import { PayPalButtons, usePayPalScriptReducer } from '@paypal/react-paypal-js'
@@ -26,7 +26,8 @@ const OrderScreen = () => {
 
   const [payOrder, { isLoading: loadingPay }] = usePayOrderMutation()
 
-  const [deliverOrder, { isLoading: loadingDeliver}] = useDeliverOrderMutation()
+  const [deliverOrder, { isLoading: loadingDeliver }] =
+    useDeliverOrderMutation()
 
   const [{ isPending }, paypalDispatch] = usePayPalScriptReducer()
 
@@ -70,28 +71,30 @@ const OrderScreen = () => {
     })
   }
 
-  async function onApproveTest() {
-    await payOrder({ orderId, details: { payer: {} } })
-    refetch()
-    toast.success('Payment successful')
-  }
+  // async function onApproveTest() {
+  //   await payOrder({ orderId, details: { payer: {} } })
+  //   refetch()
+  //   toast.success('Payment successful')
+  // }
 
   function onError(err) {
-    toast.error(err.message);
+    toast.error(err.message)
   }
 
   function createOrder(data, actions) {
-    return actions.order.create({
-      purchase_units: [
-        {
-          amount: {
-            value: order.totalPrice
+    return actions.order
+      .create({
+        purchase_units: [
+          {
+            amount: {
+              value: order.totalPrice
+            }
           }
-        }
-      ]
-    }).then((orderId) => {
-      return orderId
-    })
+        ]
+      })
+      .then(orderId => {
+        return orderId
+      })
   }
 
   const deliverOrderHandler = async () => {
@@ -224,15 +227,22 @@ const OrderScreen = () => {
                 </ListGroup.Item>
               )}
 
-              { loadingDeliver && <Loader /> }
+              {loadingDeliver && <Loader />}
 
-              { userInfo && userInfo.isAdmin && order.isPaid && !order.isDelivered && (
-                <ListGroup.Item>
-                  <Button type='button' className='btn btn-block' onClick={deliverOrderHandler}>
-                    Mark As Delivered
-                  </Button>
-                </ListGroup.Item>
-              ) }
+              {userInfo &&
+                userInfo.isAdmin &&
+                order.isPaid &&
+                !order.isDelivered && (
+                  <ListGroup.Item>
+                    <Button
+                      type='button'
+                      className='btn btn-block'
+                      onClick={deliverOrderHandler}
+                    >
+                      Mark As Delivered
+                    </Button>
+                  </ListGroup.Item>
+                )}
             </ListGroup>
           </Card>
         </Col>
